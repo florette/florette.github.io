8aa76f3244396ab7a08270eeae60d809b2b0bb9d
Flore Laforge <flore.laforge@westpac.com.au>
2018-08-20 15:05:33 +1000

    Remove audio, and just keep presentation for Lexi

--- lexi/scripts/script.js
+++ lexi/scripts/script.js
@@ -51,25 +51,25 @@ $(document).ready(function () {
     $radios
       .attr('checked', false);
 
-    $audio.each(function(){
-      $(this).removeClass('playing').get(0).pause();
-      $audio[0].currentTime = 0;
-      $audio[1].currentTime = 0;
-      $audio[2].currentTime = 0;
-      $audio[3].currentTime = 0;
-      $audio[4].currentTime = 0;
-    })
+    // $audio.each(function(){
+    //   $(this).removeClass('playing').get(0).pause();
+    //   $audio[0].currentTime = 0;
+    //   $audio[1].currentTime = 0;
+    //   $audio[2].currentTime = 0;
+    //   $audio[3].currentTime = 0;
+    //   $audio[4].currentTime = 0;
+    // })
 
     if (currentIndex >= radiosLength - 1) {
       $radios
         .first()
         .attr('checked', true);
 
-      $audio.currentTime = 0;
-      setTimeout(function () {
-        $audio
-          .first().addClass('playing').get(0).play();
-      }, 2000);
+      // $audio.currentTime = 0;
+      // setTimeout(function () {
+      //   $audio
+      //     .first().addClass('playing').get(0).play();
+      // }, 2000);
 
     } else {
 
@@ -77,11 +77,11 @@ $(document).ready(function () {
         .prev('input[class*="slide-radio"]')
         .attr('checked', true);
 
-      $activeAudio.currentTime = 0;
-      setTimeout(function () {
-        $activeAudio
-          .prev().addClass('playing').get(0).play();
-      }, 2000);
+      // $activeAudio.currentTime = 0;
+      // setTimeout(function () {
+      //   $activeAudio
+      //     .prev().addClass('playing').get(0).play();
+      // }, 2000);
     }
   })
 
@@ -98,96 +98,96 @@ $(document).ready(function () {
     $radios
       .attr('checked', false);
 
-    $audio.each(function(){
-      $(this).removeClass('playing').get(0).pause();
-      $audio[0].currentTime = 0;
-      $audio[1].currentTime = 0;
-      $audio[2].currentTime = 0;
-      $audio[3].currentTime = 0;
-      $audio[4].currentTime = 0;
-    })
+    // $audio.each(function(){
+    //   $(this).removeClass('playing').get(0).pause();
+    //   $audio[0].currentTime = 0;
+    //   $audio[1].currentTime = 0;
+    //   $audio[2].currentTime = 0;
+    //   $audio[3].currentTime = 0;
+    //   $audio[4].currentTime = 0;
+    // })
 
     if (currentIndex >= radiosLength - 1) {
       $radios
         .first()
         .attr('checked', true);
 
-      $audio.currentTime = 0;
-      setTimeout(function () {
-        $audio
-          .first().addClass('playing').get(0).play();
-      }, 2000);
+      // $audio.currentTime = 0;
+      // setTimeout(function () {
+      //   $audio
+      //     .first().addClass('playing').get(0).play();
+      // }, 2000);
     } else {
 
       $activeRadio
         .next('input[class*="slide-radio"]')
         .attr('checked', true);
 
-      $activeAudio.currentTime = 0;
-      setTimeout(function () {
-        $activeAudio
-          .next().addClass('playing').get(0).play();
-      }, 2000);
+      // $activeAudio.currentTime = 0;
+      // setTimeout(function () {
+      //   $activeAudio
+      //     .next().addClass('playing').get(0).play();
+      // }, 2000);
     }
   })
 
-  $('.play__presentation').click(function(){
-    $(this).removeClass('play__overlay');
-    var $audio = $('.audio__slide');
-    var $music = $('.music');
+  // $('.play__presentation').click(function(){
+  //   $(this).removeClass('play__overlay');
+  //   var $audio = $('.audio__slide');
+  //   var $music = $('.music');
 
-    $music.get(0).play();
+  //   $music.get(0).play();
 
-    setTimeout(function () {
-      $audio
-        .first().addClass('playing').get(0).play();
+  //   setTimeout(function () {
+  //     $audio
+  //       .first().addClass('playing').get(0).play();
 
-      var timeout1 = 17500;
-      var timeout2 = 37500;
-      var timeout3 = 41500;
-      var timeout4 = 16500;
-      var timeout5 = 44000;
+  //     var timeout1 = 17500;
+  //     var timeout2 = 37500;
+  //     var timeout3 = 41500;
+  //     var timeout4 = 16500;
+  //     var timeout5 = 44000;
 
-      function clickNext() {
-        $('.slider_next').trigger('click');
-        console.log("click")
-      }
+  //     function clickNext() {
+  //       $('.slider_next').trigger('click');
+  //       console.log("click")
+  //     }
 
-      console.log('slide 1')
+  //     console.log('slide 1')
 
-      setTimeout(function () {
-        console.log('slide 2')
-        clickNext()
-        setTimeout(function () {
-          console.log('slide 3')
-          clickNext()
-          setTimeout(function () {
-            console.log('slide 4')
-            clickNext()
-            setTimeout(function () {
-              console.log('slide 5')
-              clickNext()
-              setTimeout(function () {
-              }, timeout5);
-            }, timeout4);
-          }, timeout3);
-        }, timeout2);
-      }, timeout1);
-
-
-    }, 300);
+  //     setTimeout(function () {
+  //       console.log('slide 2')
+  //       clickNext()
+  //       setTimeout(function () {
+  //         console.log('slide 3')
+  //         clickNext()
+  //         setTimeout(function () {
+  //           console.log('slide 4')
+  //           clickNext()
+  //           setTimeout(function () {
+  //             console.log('slide 5')
+  //             clickNext()
+  //             setTimeout(function () {
+  //             }, timeout5);
+  //           }, timeout4);
+  //         }, timeout3);
+  //       }, timeout2);
+  //     }, timeout1);
 
 
+  //   }, 300);
 
 
 
-    // var interval1 = setInterval(clickNext, timeout1);
-    // var interval2 = setInterval(clickNext, timeout2);
-    // var interval3 = setInterval(clickNext, timeout3);
-    // var interval4 = setInterval(clickNext, timeout4);
-    // var interval5 = setInterval(clickNext, timeout5);
 
-  })
+
+  //   // var interval1 = setInterval(clickNext, timeout1);
+  //   // var interval2 = setInterval(clickNext, timeout2);
+  //   // var interval3 = setInterval(clickNext, timeout3);
+  //   // var interval4 = setInterval(clickNext, timeout4);
+  //   // var interval5 = setInterval(clickNext, timeout5);
+
+  // })
 
 
 
